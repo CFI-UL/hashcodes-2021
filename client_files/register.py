2c4b8195752b9953bebd205425e7df84e7b5ab18
Olivier Langlois <olivier.langlois@polyrix.com>
2021-03-07 22:35:40 -0500

    update client scripts to use aws ip

--- client_files/register.py
+++ client_files/register.py
@@ -1,7 +1,7 @@
 import json
 import socket
 
-address = ('localhost', 1337)
+address = ('34.234.223.110', 1337)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(address)
--- client_files/send_solution.py
+++ client_files/send_solution.py
@@ -1,7 +1,7 @@
 import json
 import socket
 
-address = ('asetin-compe.herokuapp.com', 80)
+address = ('34.234.223.110', 1337)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print(f'Connecting to {address}')
