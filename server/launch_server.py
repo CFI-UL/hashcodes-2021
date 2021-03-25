e59570cbd09a92da8dc70cb763bcfd23badc2dbe
Olivier Langlois <olivier.langlois@polyrix.com>
2021-03-25 19:52:27 -0400

    yup, I suck

--- server/launch_server.py
+++ server/launch_server.py
@@ -5,10 +5,12 @@ import json
 import exceptions
 import os
 
+from datetime import datetime
+
 def json_serial(obj):
     """JSON serializer for objects not serializable by default json code"""
 
-    if isinstance(obj, (datetime, date)):
+    if isinstance(obj, datetime):
         return obj.isoformat()
     raise TypeError ("Type %s not serializable" % type(obj))
 
